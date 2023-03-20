bf5ca38409c5dd307003b98516927e9bb73b6966
MathxH Chen <brainfvck@foxmail.com>
2023-03-20 16:48:07 +0800

    Remove useless import

--- contracts/GabbyNFTAirdrop.sol
+++ contracts/GabbyNFTAirdrop.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract GabbyNFTAirdrop is ERC721, Ownable {
